e6d98e62839e9e82fb616648d6d0754ea93c64d1
kenta <nakamori.k@gmail.com>
2018-09-30 21:32:01 +0900

    Revert "V1.2"
    This reverts commit 2ebc34a8700c3648be7e283f6172dddd3e5a0e4b.

--- todo/urls.py
+++ todo/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todos_for_user, name='home'),
-    # path('post/', views.todos_for_user, name='home'),
+    path('', views.login_page, name='login'),
+    path('post/', views.todos_for_user, name='home'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/delete/', views.TodoDeleteView.as_view(), name='post_delete'),
 ]
